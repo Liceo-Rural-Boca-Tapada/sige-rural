7bcb1dd11c1934f16d2c81c334ff59996373f017
Demer <evaluacionlrbt@gmail.com>
2026-05-20 08:39:02 -0600

    fix final prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,15 +5,26 @@ import path from "path";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: path.join("prisma", "schema.prisma"),
+
+  schema: path.join(
+    "prisma",
+    "schema.prisma"
+  ),
 
   migrations: {
-    path: path.join("prisma", "migrations"),
+    path: path.join(
+      "prisma",
+      "migrations"
+    ),
   },
 
   engine: "classic",
 
   datasource: {
-    url: process.env.DATABASE_URL || "",
+
+    url:
+      process.env.DATABASE_URL || "",
+
   },
+
 });
